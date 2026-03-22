67e7526b9eb5101b91d4d9ee1947cd715d9e9c33
Milos <milos.marjanovic159@gmail.com>
2026-03-22 23:03:35 +0100

    manage profile card

--- src/app/components/profileCard/ProfileCard.js
+++ src/app/components/profileCard/ProfileCard.js
@@ -1,14 +1,47 @@
 import React from "react";
-import { Button } from "@chakra-ui/react";
+import { Box, Avatar, Text, Divider, Button } from "@chakra-ui/react";
+import { FiLogOut } from "react-icons/fi";
 import { useAuthContext } from "../pageGuard/withAuth";
 import QRCodeLayout from "../qrCode/QRCode";
 const ProfileCard = () => {
   const { logout, user } = useAuthContext();
-
+  console.log(user, 111);
   return (
     <div>
-      Profil
-      <Button onClick={logout}>Logout</Button>
+      <Box
+        bg="white"
+        borderRadius="xl"
+        border="1px solid"
+        borderColor="gray.100"
+        p={5}
+        w="300px"
+      >
+        <Box display="flex" alignItems="center" gap={3} mb={4}>
+          <Avatar name={user?.displayName} src={user?.photoURL} size="md" />
+          <Box>
+            <Text fontWeight="600" fontSize="15px" lineHeight="1.2">
+              {user?.displayName}
+            </Text>
+            <Text fontSize="13px" color="gray.500" mt={0.5}>
+              {user?.email}
+            </Text>
+          </Box>
+        </Box>
+
+        <Divider mb={4} />
+
+        <Button
+          w="full"
+          variant="ghost"
+          colorScheme="red"
+          leftIcon={<FiLogOut />}
+          justifyContent="flex-start"
+          fontSize="14px"
+          onClick={logout}
+        >
+          Log out
+        </Button>
+      </Box>
       <QRCodeLayout user={user} />
     </div>
   );
